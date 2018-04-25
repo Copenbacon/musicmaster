8119314dda6ef9ce4b3c7c7e9030e84b3170ae04
copenbacon <sclary50@gmail.com>
2018-04-25 14:27:15 -0500

    adding some genres and genre styling

--- src/profile.jsx
+++ src/profile.jsx
@@ -4,7 +4,7 @@ import './app.css';
 class Profile extends Component {
 	render() {
 		console.log('this.props', this.props);
-		let artist = {name:'', followers: {total: '',}, images: [{url: ''}]};
+		let artist = {name:'', followers: {total: '',}, images: [{url: ''}], genres:[]};
 		if(this.props.artist !== null){
 			artist = this.props.artist;
 		}
@@ -18,6 +18,16 @@ class Profile extends Component {
 				/>
 				<div>{artist.name}</div>
 				<div>{artist.followers.total}</div>
+				<div>
+					{
+						artist.genres.map((genre, index) => {
+							genre = genre !== artist.genres[artist.genres.length - 1] ? ` ${genre} |` : ` ${genre}`
+							return (
+								<span key={index}>{genre}</span>
+							);
+						})
+					}
+				</div>
 			</div>
 
 		)
